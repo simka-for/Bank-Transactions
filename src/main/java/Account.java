9868ab9c704b367a835ae000a8f9bce4ec3f0d2d
Simka-for <58855519+simka-for@users.noreply.github.com>
2020-03-05 21:09:46 +0300

    tests failed

--- src/main/java/Account.java
+++ src/main/java/Account.java
@@ -1,14 +1,55 @@
-public class Account
-{
-    private long balance;
+public class Account {
+    private long money;
+    private STATE state;
+    private String accNumber;
 
-
-    public void transfer(Account account1, Account account2, long amount) {
-
+    public Account(long money, String accNumber) {
+        this.money = money;
+        this.accNumber = accNumber;
+        this.state = STATE.NORMAL;
     }
 
-    public void getBalance(){
+    public synchronized long getMoney() {
+        return money;
+    }
 
+    public synchronized void raise(long amount) {
+        if (money > amount) {
+            money -= amount;
+        } else {
+            System.out.println("Can't finish operation: not enough money");
+        }
+    }
+
+    public synchronized void put(long amount) {
+        money += amount;
+    }
+
+    public synchronized boolean isBusy() {
+        return this.state == STATE.BUSY;
+    }
+
+    public synchronized boolean isLocked() {
+        return this.state == STATE.LOCKED;
+    }
+
+    public synchronized boolean isNormal() {
+        return state == STATE.NORMAL;
+    }
+
+    public synchronized void setBusy() {
+        this.state = STATE.BUSY;
+    }
+
+    public synchronized void setLocked() {
+        this.state = STATE.LOCKED;
+    }
+
+    public synchronized void setNormal() {
+        this.state = STATE.NORMAL;
+    }
+
+    enum STATE {
+        LOCKED, BUSY, NORMAL
     }
 }
-
--- src/main/java/Bank.java
+++ src/main/java/Bank.java
@@ -1,18 +1,125 @@
-import java.util.HashMap;
 import java.util.Random;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
-public class Bank
-{
+public class Bank {
 
     private final Random random = new Random();
+    private ConcurrentHashMap<String, Account> accounts;
 
-    public synchronized boolean isFraud(String fromAccountNum, String toAccountNum, long amount)
-            throws InterruptedException
-    {
+    public Bank() {
+        accounts = new ConcurrentHashMap<>();
+    }
+
+    private synchronized boolean isFraud(String fromAccountNum, String toAccountNum, long amount)
+            throws InterruptedException {
         Thread.sleep(1000);
-        return random.nextBoolean();
+        return chanceBlock();
+    }
+
+    public void addAccount(String accNumber, long money) {
+        accounts.put(accNumber, new Account(money, accNumber));
     }
 
 
+    public void transfer(String fromAccountNum, String toAccountNum, long amount) throws InterruptedException {
+        Account fromAccount = getAccount(fromAccountNum);
+        Account toAccount = getAccount(toAccountNum);
 
+        if (fromAccount == null) {
+            System.err.println(String.format("Account #%s was not found", fromAccountNum));
+            return;
+        }
+        if (toAccount == null) {
+            System.err.println(String.format("Account #%s was not found", toAccountNum));
+            return;
+        }
+
+        boolean isBusy = fromAccount.isBusy() || toAccount.isBusy();
+        if (isBusy)
+            System.out.println("At least one account is busy. Should wait...");
+        while (isBusy) {
+            isBusy = fromAccount.isBusy() || toAccount.isBusy();
+        }
+
+        Lock lock1 = new ReentrantLock();
+        Lock lock2 = new ReentrantLock();
+
+        takeLocks(lock1, lock2);
+        try{
+            if (fromAccount.isLocked() || toAccount.isLocked()) {
+                System.out.println(fromAccount.isLocked() ? toAccount.isLocked() ?
+                        String.format("Accounts #%s and #%s are locked", fromAccountNum, toAccountNum) :
+                        String.format("Account #%s is locked", fromAccountNum) :
+                        String.format("Account #%s is locked", toAccountNum));
+            }
+            if (fromAccount.isNormal() && toAccount.isNormal()) {
+                fromAccount.setBusy();
+                toAccount.setBusy();
+
+                if (amount > 50000 && isFraud(fromAccountNum, toAccountNum, amount)) {
+                    fromAccount.setLocked();
+                    toAccount.setLocked();
+                    System.out.println(String.format("Transfer %1$s from account #%2$s to #%3$s not finished and " +
+                                    "in result of " +
+                                    "security check accounts #%2$s and #%3$s are locked", amount, fromAccountNum,
+                            toAccountNum));
+                } else {
+                    fromAccount.setNormal();
+                    toAccount.setNormal();
+                    System.out.println(String.format("Transfer %s from %s to %s is finished successfully", amount
+                            , fromAccountNum, toAccountNum));
+                }
+                if (getBalance(fromAccountNum) >= amount && fromAccount.isNormal() && toAccount.isNormal()) {
+                    fromAccount.raise(amount);
+                    toAccount.put(amount);
+                }
+            }
+        }finally {
+            lock1.unlock();
+            lock2.unlock();
+        }
+        }
+
+    private void takeLocks(Lock lock1, Lock lock2) throws InterruptedException {
+        boolean firstLock = false;
+        boolean secondLock = false;
+
+        while(true){
+            try{
+                firstLock = lock1.tryLock();
+                secondLock = lock2.tryLock();
+            }finally {
+                if (firstLock && secondLock){
+                    return;
+                }
+                if (firstLock){
+                    lock1.unlock();
+                }
+                if (secondLock){
+                    lock2.unlock();
+                }
+                Thread.sleep(1);
+            }
+        }
+    }
+
+    public Account getAccount(String number) {
+        return accounts.get(number);
+    }
+
+    public long getBalance(String accountNum) {
+        return getAccount(accountNum).getMoney();
+    }
+
+    public ConcurrentHashMap<String, Account> getAccounts() {
+        return accounts;
+    }
+
+    private boolean chanceBlock(){     // chance of locking
+        double grade = Math.random();
+
+        return !(grade < 0.75);
+    }
 }
